39f2c9e09c0cb5387c8719ce5a120721bf913b2e
Andrew MacDonald <awm@alum.mit.edu>
2019-02-16 15:05:58 -0800

    Parser helper routines (section 2.3)

--- toy.cpp
+++ toy.cpp
@@ -1,6 +1,25 @@
 // Kaleidoscope language from LLDB
+#include <memory>
 #include <string>
 #include <vector>
+
+/******** PREDECLARATIONS *******/
+
+class ExprAST;
+class PrototypeAST;
+
+
+/******** HELPER ROUTINES *******/
+
+/// LogError* - These are little helper functions for error handling.
+std::unique_ptr<ExprAST> LogError(const char *Str) {
+  fprintf(stderr, "LogError: %s\n", Str);
+  return nullptr;
+}
+std::unique_ptr<PrototypeAST> LogErrorP(const char *Str) {
+  LogError(Str);
+  return nullptr;
+}
 
 /********* LEXER **********/
 
@@ -146,3 +165,14 @@ public:
               std::unique_ptr<ExprAST> Body)
     : Proto(std::move(Proto)), Body(std::move(Body)) {}
 };
+
+
+/********** PARSER ***********/
+
+/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
+/// token the parser is looking at.  getNextToken reads another token from the
+/// lexer and updates CurTok with its results.
+static int CurTok;
+static int getNextToken() {
+  return CurTok = gettok();
+}
